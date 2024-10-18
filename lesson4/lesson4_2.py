199f12071331270a82b05157b59517123f847a3f
JitKhin <dana_sakura@hotmail.com>
2024-10-18 13:33:02 +0800

    lesson4_08

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -5,16 +5,24 @@ class Window(tk.Tk):
     #     super().__init__(screenName=screenName,baseName=baseName,className=className,useTk=useTk,sync=sync,use=use)
      def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.title('這是我的第一個視窗')
+        self.geometry('800x300')
+        messege=tk.Label(self,text='Hello! 這是我的第一個視窗')
+        messege.pack()
 
 def main():
     # root = tk.Tk()
-    root = Window()
-    print(type(root))
-    root.title('這是我的第一個視窗')
-    root.geometry('800x300')
-    messege=tk.Label(root,text='Hello! 這是我的第一個視窗')
-    messege.pack()
-    root.mainloop()
+
+    # root = Window()
+    # print(type(root))
+    # root.title('這是我的第一個視窗')
+    # root.geometry('800x300')
+    # messege=tk.Label(root,text='Hello! 這是我的第一個視窗')
+    # messege.pack()
+    # root.mainloop()
+
+    window=Window()
+    window.mainloop()
 
 if __name__ == '__main__':
     main()
