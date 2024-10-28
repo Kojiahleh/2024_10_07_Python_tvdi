5ae53990f52f15a37b6c9769cf2c91993d0af80a
JitKhin <dana_sakura@hotmail.com>
2024-10-28 14:06:56 +0800

    lesson6_05

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -27,6 +27,7 @@ class Window(ThemedTk):
         self.selected_site = tk.StringVar()
         sitenames_cb = ttk.Combobox(bottomFrame, textvariable=self.selected_site,values=sitenames,state='readonly')
         self.selected_site.set('請選擇站點')
+        sitenames_cb.bind('<<ComboboxSelected>>', self.sitename_selected)
         sitenames_cb.pack(side='left',expand=True,anchor='n')
 
         # define columns
@@ -52,12 +53,8 @@ class Window(ThemedTk):
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
         #==============end bottomFrame===============
 
-    def agreement_changed(self):
-        showinfo(
-            title='站點',
-            message= self.selected_site.set()
-
-        )
+    def sitename_selected(self,event):
+        print(self.selected_site.get())
 
 
     
