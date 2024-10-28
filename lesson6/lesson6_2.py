a6329e5f9a037e6d8bee3c828bc080e93717efe9
JitKhin <dana_sakura@hotmail.com>
2024-10-28 14:40:58 +0800

    lesson6_06

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -2,7 +2,6 @@ import datasource
 from tkinter import ttk
 import tkinter as tk
 from ttkthemes import ThemedTk
-from tkinter.messagebox import showinfo
 
 class Window(ThemedTk):
     def __init__(self,*args, **kwargs):
@@ -31,23 +30,36 @@ class Window(ThemedTk):
         sitenames_cb.pack(side='left',expand=True,anchor='n')
 
         # define columns
-        columns = ('first_name', 'last_name', 'email')
+        columns = ('date', 'county', 'aqi', 'pm25', 'status', 'lat', 'lon')
 
         tree = ttk.Treeview(bottomFrame, columns=columns, show='headings')
 
         # define headings
-        tree.heading('first_name', text='First Name')
-        tree.heading('last_name', text='Last Name')
-        tree.heading('email', text='Email')
+        tree.heading('date', text='日期')
+        tree.heading('county', text='縣市')
+        tree.heading('aqi', text='AQI')
+        tree.heading('pm25', text='PM25')
+        tree.heading('status', text='狀態')
+        tree.heading('lat', text='緯度')
+        tree.heading('lon', text='經度')
 
-        # generate sample data
-        contacts = []
-        for n in range(1, 100):
-            contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+        tree.column('date', width=150,anchor='center')
+        tree.column('county', width=80,anchor='center')
+        tree.column('aqi', width=50,anchor='center')
+        tree.column('pm25', width=50,anchor='center')
+        tree.column('status', width=50,anchor='center')
+        tree.column('lat', width=100,anchor='center')
+        tree.column('lon', width=100,anchor='center')
+        tree.insert("", "end", values=('2024-10-28 09:00','屏東縣',17,6.5,'良好','22.260899','120.651472'))
 
-        # add data to the treeview
-        for contact in contacts:
-            tree.insert('', tk.END, values=contact)
+        # # generate sample data
+        # contacts = []
+        # for n in range(1, 100):
+        #     contacts.append((f'first {n}', f'last {n}', f'email{n}@example.com'))
+
+        # # add data to the treeview
+        # for contact in contacts:
+        #     tree.insert('', tk.END, values=contact)
 
         tree.pack(side='right')
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
@@ -60,7 +72,7 @@ class Window(ThemedTk):
     
     
 def main():
-    window = Window(theme="breeze")
+    window = Window(theme="radiant")
     window.mainloop()
 
 if __name__ == '__main__':
