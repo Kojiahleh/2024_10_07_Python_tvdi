d70d08baff69ee0e6348ace911cf550f43444560
JitKhin <dana_sakura@hotmail.com>
2024-11-13 16:19:15 +0800

    lesson9_16

--- lesson9/view/item_dialog.py
+++ lesson9/view/item_dialog.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.simpledialog import Dialog
+from PIL import Image, ImageTk
+import tkintermapview as tkmap
 
 class MyCustomDialog(Dialog):
     def __init__(self,parent,record:list,title=None):
@@ -17,19 +19,62 @@ class MyCustomDialog(Dialog):
 
     def body(self, master):
         # 創建對話框主體。返回應具有初始焦點的控件。
+        ttk.Label(master, text=self.date, font=("Helvetica",24,"bold"), foreground='#724832').pack(pady=10)
+
         main_frame = ttk.Frame(master,borderwidth=1,relief='groove')
-        ttk.Label(main_frame,text=self.status).pack()
-        canvas_left = tk.Canvas(main_frame,width=200,height=200)
-        canvas_left.create_rectangle(10,10,190,190,outline="#9E7A7A",width=2)
-        #canvas_left.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        canvas_left.create_text(100, 40, text=f'AQI:{self.status}',font=("Helvetica",24,"bold"),fill='#9E7A7A')
+        
+        canvas_left = tk.Canvas(main_frame,width=200,height=200,background='#1E88A8')
+        
+        if self.aqi <= 50:
+            path = './images/green-light.png'
+            self.aqi_status = '良好'
+        elif self.aqi <= 100:
+            path = './images/orange-light.png'
+            self.aqi_status = '普通'
+        else:
+            self.aqi_status = '危險'
+            path = './images/red-light.png' 
+
+        canvas_left.create_rectangle(10,10,190,190,outline="#FEDFE1",width=2)
+        canvas_left.create_text(100, 40, text=f'AQI:{self.aqi_status}', font=("Helvetica",20,"bold"), fill='#FEDFE1')
+        aqi_img = Image.open(path)
+        self.resize_image = aqi_img.resize((75,75))
+        self.aqi_light = ImageTk.PhotoImage(self.resize_image)
+        canvas_left.create_image(100, 100, anchor='center', image=self.aqi_light)
+        canvas_left.create_text(100, 160, text=f'AQI:{self.aqi}', font=("Helvetica",20,"bold"), fill='#FEDFE1')  
+        
         canvas_left.pack(side='left')
 
-        # canvas_right = tk.Canvas(main_frame,width=200,height=200)
-        # canvas_right.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
-        # canvas_right.create_text(200, 150, text=self.status,font=("Helvetica",24,"bold"),fill='#9E7A7A')
-        # canvas_right.pack(side='right')
+        canvas_right = tk.Canvas(main_frame,width=200,height=200,background='#1E88A8')
+        
+        if self.pm25 <= 15.4:
+            path = './images/green-light.png'
+            self.pm25_status = '良好'
+        elif self.pm25 <= 35.4:
+            path = './images/orange-light.png'
+            self.pm25_status = '普通'
+        else:
+            self.pm25_status = '危險'
+            path = './images/red-light.png' 
+
+        canvas_right.create_rectangle(10,10,190,190,outline="#FEDFE1",width=2)
+        canvas_right.create_text(100, 40, text=f'PM2.5:{self.pm25_status}', font=("Helvetica",20,"bold"), fill='#FEDFE1')
+        pm25_img = Image.open(path)
+        self.resize_image = pm25_img.resize((75,75))
+        self.pm25_light = ImageTk.PhotoImage(self.resize_image)
+        canvas_right.create_image(100, 100, anchor='center', image=self.pm25_light)   
+        canvas_right.create_text(100, 160, text=f'PM2.5:{self.pm25}', font=("Helvetica",20,"bold"), fill='#FEDFE1')   
+        
+        canvas_right.pack(side='right')
+
         main_frame.pack(expand=True,fill='x')
+
+        map_frame = ttk.Frame(master)
+        map_widget = tkmap.TkinterMapView(map_frame, width=400, height=400, corner_radius=0)
+        map_widget.set_position(self.lat, self.lon,marker=True) #台北市位置
+        map_widget.set_zoom(15) #設定顯示大小
+        map_widget.pack()
+        map_frame.pack(padx=10,pady=10)
 
     def apply(self):
         # 當用戶按下確定時處理數據
